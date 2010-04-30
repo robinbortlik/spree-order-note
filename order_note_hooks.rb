1604c202099c9e579b8a02843e15765e1207a96d
robinbortlik <robinbortlik@gmail.com>
2010-04-30 16:44:19 +0200

    add note detail to product detail in administration

--- order_note_hooks.rb
+++ order_note_hooks.rb
@@ -1,4 +1,5 @@
 class OrderNoteHooks < Spree::ThemeSupport::HookListener
   insert_before :checkout_summary_box, 'shared/pre_order_note'
   insert_after :checkout_summary_box, 'shared/order_note'
+  insert_before :admin_order_show_details, 'shared/order_note_detail'
 end
